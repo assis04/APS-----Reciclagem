6f169e59c71fdb7d7a7d90505cab68ac50f86f3b
Assis04 <thiagoassis642@gmail.com>
2025-10-23 20:32:27 -0300

    Commit para dar pull

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,141 +12,146 @@ import { Box, TextField, Typography, Grid, Link, Stack } from "@mui/material";
 import Loading from "./Commons/Component/Loading/loading";
 
 const HomePage = () => {
-  const router = useRouter();
+    const router = useRouter();
 
-  const [senha, setSenha] = useState("");
-  const [email, setUsuario] = useState("");
-  const [loading, setLoading] = useState(false);
+    const [senha, setSenha] = useState("");
+    const [email, setUsuario] = useState("");
+    const [loading, setLoading] = useState(false);
 
-  const goToHome = () => {
-    setTimeout(() => {
-      router.push("/PagesRouter/Home");
-      setLoading(true);
-    }, 1000);
-  };
-  const handleLogin = async () => {
-    setLoading(true);
+    const goToHome = () => {
+        setTimeout(() => {
+            router.push("/PagesRouter/Home");
+            setLoading(true);
+        }, 1000);
+    };
+    const handleLogin = async () => {
+        setLoading(true);
 
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-    try {
-      const response = await fetch(`${apiUrl}/api/usuarios/login`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email: email, senha: senha }),
-      });
+        try {
+            const response = await fetch(`${apiUrl}/api/usuarios/login`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email: email, senha: senha }),
+            });
 
-      if (response.ok) {
-        toast.success("Usuário logado com sucesso!");
+            if (response.ok) {
+                toast.success("Usuário logado com sucesso!");
+
+                setTimeout(() => {
+                    router.push("/PagesRouter/Home");
+                }, 1000);
+            } else {
+                const errorMsg = await response.text();
+                toast.error(errorMsg);
+                setLoading(false);
+            }
+        } catch (error) {
+            toast.error("Erro ao conectar com o servidor");
+            setLoading(false);
+        }
+    };
+
+    const goToRegister = () => {
+        setLoading(true);
 
         setTimeout(() => {
-          router.push("/PagesRouter/Home");
+            router.push("/PagesRouter/Register");
+            setLoading(true);
         }, 1000);
-      } else {
-        const errorMsg = await response.text();
-        toast.error(errorMsg);
-        setLoading(false);
-      }
-    } catch (error) {
-      toast.error("Erro ao conectar com o servidor");
-      setLoading(false);
-    }
-  };
+    };
 
-  const goToRegister = () => {
-    setLoading(true);
+    const goToRecoveryPassword = () => {
+        setLoading(true);
 
-    setTimeout(() => {
-      router.push("/PagesRouter/Register");
-      setLoading(true);
-    }, 1000);
-  };
+        setTimeout(() => {
+            router.push("/PagesRouter/Password-recovery");
+        }, 1000);
+    };
 
-  const goToRecoveryPassword = () => {
-    setLoading(true);
-
-    setTimeout(() => {
-      router.push("/PagesRouter/Password-recovery");
-    }, 1000);
-  };
-
-  return (
-    <>
-      <header>
-        <DefaultNavBar />
-      </header>
-      <main>
-        <Box
-          sx={{
-            display: "flex",
-            minWidth: "100vw",
-            height: "calc(100vh - 5rem)",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundImage: 'url("/BackgroundAPS.svg")',
-            position: "relative", // necessário para overlay funcionar
-          }}
-        >
-          <Grid
-            container
-            justifyContent={"space-around"}
-            gap={3}
-            backgroundColor={"black"}
-            width={"60vw"}
-          >
-            <Grid size={5}>
-              <Stack spacing={2}>
+    return (
+        <>
+            <header>
+                <DefaultNavBar />
+            </header>
+            <main>
                 <Box
-                  sx={{
-                    backgroundColor: "aqua",
-                    height: "31rem",
-                    width: "30rem",
-                    borderRadius: "2rem",
-                    padding: "3rem",
-                  }}
+                    sx={{
+                        display: "flex",
+                        minWidth: "100vw",
+                        height: "calc(100vh - 5rem)",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        backgroundImage: 'url("/BackgroundAPS.svg")',
+                        position: "relative", // necessário para overlay funcionar
+                    }}
                 >
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-                  eius aliquid saepe est nihil! Illum perferendis quaerat unde,
-                  delectus alias quia, optio dolorum corporis ipsa neque
-                  aspernatur, est repellat enim?
+                    <Grid
+                        container
+                        justifyContent={'center'}
+                        alignItems={'center'}
+                        width={"60vw"}
+                        gap={2}
+                    >
+                        <Grid item size={5}>
+                            <Stack spacing={2}>
+                                <Box
+                                    sx={{
+                                        backgroundColor: "#73946B",
+                                        height: "49.6vh",
+                                        width: "25vw",
+                                        border: '3px  solid rgb(255,255,255,0.2)',
+                                        padding: "3rem",
+                                        borderRadius: 5,
+                                    }}
+                                >
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                                    eius aliquid saepe est nihil! Illum perferendis quaerat unde,
+                                    delectus alias quia, optio dolorum corporis ipsa neque
+                                    aspernatur, est repellat enim?
+                                </Box>
+                            </Stack>
+                        </Grid>
+                        <Grid item size={5}>
+                            <Stack spacing={2}>
+                                <Box
+                                    sx={{
+                                        backgroundColor: "#73946B",
+                                        height: "16rem",
+                                        width: "30rem",
+                                        borderRadius: "2rem",
+                                        padding: "3rem",
+                                        border: '3px  solid rgb(255,255,255,0.2)',
+
+                                    }}
+                                >
+                                    Texto 2
+                                </Box>
+                                <Box
+                                    sx={{
+                                        backgroundColor: "#73946B",
+                                        height: "15rem",
+                                        width: "30rem",
+                                        borderRadius: "2rem",
+                                        padding: "3rem",
+                                        border: '3px  solid rgb(255,255,255,0.2)',
+
+                                    }}
+                                >
+                                    Texto 3
+                                </Box>
+                            </Stack>
+                        </Grid>
+                    </Grid>
                 </Box>
-              </Stack>
-            </Grid>
-            <Grid size={5}>
-              <Stack spacing={2}>
-                <Box
-                  sx={{
-                    backgroundColor: "aqua",
-                    height: "15rem",
-                    width: "30rem",
-                    borderRadius: "2rem",
-                    padding: "3rem",
-                  }}
-                >
-                  Texto 2
-                </Box>
-                <Box
-                  sx={{
-                    backgroundColor: "aqua",
-                    height: "15rem",
-                    width: "30rem",
-                    borderRadius: "2rem",
-                    padding: "3rem",
-                  }}
-                >
-                  Texto 3
-                </Box>
-              </Stack>
-            </Grid>
-          </Grid>
-        </Box>
-        <ToastContainer />
-      </main>
-      <footer></footer>
-    </>
-  );
+                <ToastContainer />
+            </main>
+            <footer></footer>
+        </>
+    );
 };
 
 export default HomePage;
