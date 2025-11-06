e84f902c058de4cac36df1faa0e16889233efa8e
Assis04 <thiagoassis642@gmail.com>
2025-11-05 23:59:12 -0300

    Refatorando

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 📸 Servir arquivos da pasta uploads
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
-
 // 🔗 Rotas de produtos
 app.use("/api/produtos", productRoutes);
 
